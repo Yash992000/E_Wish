39d7186a22d01063593765ffbb8150638e7f6607
Yash992000 <imsc200047yash@ljku.edu.in>
2023-04-20 14:03:23 +0530

    product completed - merchant side

--- merchant/urls.py
+++ merchant/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('merchant_index',views.merchant_index,name='merchant_index'),
     path('merchant_add_products',views.merchant_add_products,name='merchant_add_products'),
     path('merchant_manage_product',views.merchant_manage_product,name='merchant_manage_product'),
+    path('deleteproduct<id>',views.deleteproduct,name='deleteproduct'),
     path('merchant_profile',views.merchant_profile,name='merchant_profile'),
     path('merchant_profile_update',views.merchant_profile_update,name='merchant_profile_update'),
     path('merchant_profile_edit/<id>',views.merchant_profile_edit,name='merchant_profile_edit'),
