b33689d440618aa5a7a85325b28cb7b445775d5c
Yash992000 <imsc200047yash@ljku.edu.in>
2023-04-23 13:00:32 +0530

    edit url

--- merchant/urls.py
+++ merchant/urls.py
@@ -5,8 +5,7 @@ from . import views
 urlpatterns = [
     path('merchant_index',views.merchant_index,name='merchant_index'),
     path('merchant_add_products',views.merchant_add_products,name='merchant_add_products'),
-    path('merchant_update_product/<id>',views.merchant_update_product,name='merchant_update_product'),
-    path('ediproduct/<id>',views.editproduct,name='editproduct'),
+    path('merchant_update_product<id>',views.merchant_update_product,name='merchant_update_product'),
     path('merchant_manage_product',views.merchant_manage_product,name='merchant_manage_product'),
     path('deleteproduct<id>',views.deleteproduct,name='deleteproduct'),
     path('merchant_profile',views.merchant_profile,name='merchant_profile'),
@@ -17,6 +16,6 @@ urlpatterns = [
     path('merchant_logout',views.merchant_logout,name='merchant_logout'),
 
     path('addProduct',views.addProduct,name='addProduct'),
-
+    path('editproduct<id>',views.editproduct,name='editproduct'),
 
 ]
