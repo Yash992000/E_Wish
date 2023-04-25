740482208678fc86be1c64eb53bac5b55fc5be34
Yash992000 <imsc200047yash@ljku.edu.in>
2023-04-25 19:49:03 +0530

    User Template
    Template Changed
    Show Image In Shop and Gallery

--- user/urls.py
+++ user/urls.py
@@ -4,14 +4,14 @@ from . import views
 urlpatterns = [
     path('index',views.index,name='index'),
     path('about',views.about,name='about'),
-    path('cart',views.cart,name='cart'),
-    path('checkout',views.checkout,name='checkout'),
+    # path('cart',views.cart,name='cart'),
+    # path('checkout',views.checkout,name='checkout'),
     path('contact_us',views.contact_us,name='contact_us'),
     path('gallery',views.gallery,name='gallery'),
-    path('my_account',views.my_account,name='my_account'),
-    path('shop_detail',views.shop_detail,name='shop_detail'),
+    # path('my_account',views.my_account,name='my_account'),
+    # path('shop_detail',views.shop_detail,name='shop_detail'),
     path('shop',views.shop,name='shop'),
-    path('wishlist',views.wishlist,name='wishlist'),
-    path('userlogout',views.userlogout,name='userlogout'),
+    # path('wishlist',views.wishlist,name='wishlist'),
+    #path('userlogout',views.userlogout,name='userlogout'),
     
 ]
--- user/views.py
+++ user/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from merchant.models import product
+
 #from auth_app.forms import UserRegistrationForm  
 #from auth_app.models import user_registration  
 
@@ -10,39 +12,41 @@ def index(request):
 def about(request):
     return render(request,'about.html')
 
-def cart(request):
-    return render(request,'cart.html')
+# def cart(request):
+#     return render(request,'cart.html')
 
-def checkout(request):
-    return render(request,'checkout.html')
+# def checkout(request):
+#     return render(request,'checkout.html')
 
 def contact_us(request):
-    return render(request,'contact_us.html')
+    return render(request,'contact.html')
 
 def gallery(request):
-    return render(request,'gallery.html')
+    obj = product.objects.all()
+    return render(request,'gallery.html',{'image' : obj})
 
-def my_account(request):
-    return render(request,'my_account.html')
+# def my_account(request):
+#     return render(request,'my_account.html')
 
-def shop_detail(request):
-    return render(request,'shop_detail.html')
+# def shop_detail(request):
+#     return render(request,'shop_detail.html')
 
 def shop(request):
-    return render(request,'shop.html')
+    obj = product.objects.all()
+    return render(request,'shop.html',{'product' : obj})
 
-def wishlist(request):
-    return render(request,'wishlist.html')
+# def wishlist(request):
+#     return render(request,'wishlist.html')
 
-def userlogout(request):
-    try:
-        del request.session['username'] 
-        del request.session['email'] 
-        del request.session['contact'] 
-        del request.session['shopName'] 
-        del request.session['shopAddr'] 
-        del request.session['shopPhone'] 
-        return render(request,'login.html')
-    except Exception as e:
-        return HttpResponse(e)
-    return render(request,'merchant_login.html')
+# def userlogout(request):
+#     try:
+#         del request.session['username'] 
+#         del request.session['email'] 
+#         del request.session['contact'] 
+#         del request.session['shopName'] 
+#         del request.session['shopAddr'] 
+#         del request.session['shopPhone'] 
+#         return render(request,'login.html')
+#     except Exception as e:
+#         return HttpResponse(e)
+#     return render(request,'merchant_login.html')
